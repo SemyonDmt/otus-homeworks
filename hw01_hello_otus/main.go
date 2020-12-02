f7847e0ad2eed795ec402c99071c94932f50d917
Semyon <semen.github@gmail.com>
2020-12-02 11:54:08 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/golang/example/stringutil"
 )
 
 func main() {
-	str := "Hello, OTUS!"
-
+	str := "Hello, OTUS! "
 	fmt.Println(stringutil.Reverse(str))
 }
